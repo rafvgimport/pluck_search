856709a6ae6aeff2134ad86cd54880dbf4354c7a
RAFVG import <rafvg.import@gmail.com>
2015-03-27 10:16:11 +0100

    Update and rename es.php to it.php

---
+++ lang/it.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @brief Search Language dictionary;
+*
+* @fileinfo: data/modules/search/lang/it.php
+*/
+$lang['search']['admin_center'] = 'Amministrazione del modulo di ricerca';
+$lang['search']['title'] = 'Amministrare il modulo di ricerca';
+$lang['search']['search'] = 'cerca';
+$lang['search']['label'] = 'cerca...';
+$lang['search']['info'] = 'info';
+$lang['search']['results'] = 'risultati';
+$lang['search']['no results'] = 'nessun risultato';
+$lang['search']['results for'] = 'risultati per';
+$lang['search']['enter a larger search term'] = 'inserisci un termine di ricerca più lungo';
+$lang['search']['cannot search symbols'] = 'non si possono cercare simboli';
+?>
